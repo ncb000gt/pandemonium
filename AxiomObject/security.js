f1a0151866ad9188eb7a902cdfef55356684bc86
Nick <nicholas.j.campbell@gmail.com>
2009-06-04 21:29:10 -0400

    Moved User pieces to a new module 'the-inn'.

--- AxiomObject/security.js
+++ AxiomObject/security.js
@@ -1,3 +1,7 @@
-function isAllowed() {
+function isAllowed(action, roles) {
+    if (roles && roles.contains('Administrator')) {
+	return true;
+    }
+
     return false;
 }
--- AxiomObject/unauthed.js
+++ AxiomObject/unauthed.js
@@ -1,3 +1,3 @@
 function unauthorized() {
-    return "hi";
+    return root.get('home').login();
 }
